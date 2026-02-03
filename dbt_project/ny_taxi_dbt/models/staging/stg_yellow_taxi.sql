925f8ced2333e8c8a490b7ad08d5c6c640c1785d
Adedayo <adeboadedayo01@gmail.com>
2026-02-03 13:03:49 +0100

    changes

--- dbt_project/ny_taxi_dbt/models/staging/stg_yellow_taxi.sql
+++ dbt_project/ny_taxi_dbt/models/staging/stg_yellow_taxi.sql
@@ -1,15 +1,15 @@
 
 select
-    vendor_id,
-    pickup_datetime,
-    dropoff_datetime,
-    pickup_location_id,
-    dropoff_location_id,
-    passenger_count,
-    trip_distance,
-    fare_amount,
-    tip_amount,
-    total_amount,
-    payment_type
-from {{ source('staging', 'yellow_taxi') }}
+    vendorid as vendor_id,
+    tpep_pickup_datetime as pickup_datetime,
+    tpep_dropoff_datetime as dropoff_datetime,
+    pulocatioid as pickup_location_id,
+    dolocatioid as dropoff_location_id,
+    passenger_count as passenger_count,
+    trip_distance as trip_distance,
+    fare_amount as fare_amount,
+    tip_amount as tip_amount,
+    total_amount as total_amount,
+    payment_type as payment_type
+from {{ source('raw', 'yellow_taxi_data') }}
 
